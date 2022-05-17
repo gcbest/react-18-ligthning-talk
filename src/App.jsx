34959ea29f7cecd41f04a3b3b93d213a6b2039fd
Godfrey Best <godfrey.c.best@gmail.com>
2022-05-17 01:32:53 -0400

    alice & bob

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,27 @@ import './App.css'
 function App() {
   console.log('COMPONENT UPDATED');
   const [count, setCount] = useState(0);
+  const [selectedPerson, setSelectedPerson] = useState('');
 
   const handleBatchClick = () => {
     setCount(count => count + 1);
-    setCount(count => count + 1);
+    setSelectedPerson('Alice');
   }
 
   const handleSeparateClick = () => {
     setTimeout(() => {
       setCount(count => count + 1);
-      setCount(count => count + 1);
+      setSelectedPerson('Bob');
     }, 100);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleBatchClick}>Batched Updates</button>
-        <button onClick={handleSeparateClick}>Individual Updates</button>
+        <div>Total # clicks: {count} </div>
+        <div>Person Selected: {selectedPerson}</div>
+        <button onClick={handleBatchClick}>Alice (Batched)</button>
+        <button onClick={handleSeparateClick}>Bob (Unbatched)</button>
       </header>
 
     </div>
